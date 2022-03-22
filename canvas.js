14f1f93a290268f3d8271ea119ecd9b703a5d39e
anuranj <anuranj888@gmail.com>
2022-03-22 22:10:18 +0530

    Basic Draw added

---
+++ canvas.js
@@ -0,0 +1,50 @@
+window.addEventListener('load', ()=>{
+    const canvas = document.querySelector("#canvas");
+    const ctx = canvas.getContext("2d");
+
+
+    //Resizing 
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth; 
+
+
+    //variables
+    let painting = false;
+
+    function startPosition(){
+        painting = true
+        draw(e);
+    }
+    function finishedPosition(){
+        painting = false;
+        ctx.beginPath()
+    }
+
+    function draw(e) {
+        if(!painting) return;
+        ctx.lineWidth = 5;
+        ctx.lineCap = "round";
+        ctx.strokeStyle = "red"
+
+        ctx.lineTo(e.clientX, e.clientY)
+        ctx.stroke()
+        ctx.beginPath();
+        ctx.moveTo(e.clientX, e.clientY)
+    }
+
+
+    //Event listeners
+    canvas.addEventListener('mousedown', startPosition); 
+    canvas.addEventListener('mouseup', finishedPosition); 
+    canvas.addEventListener('mousemove', draw); 
+
+
+    /* ctx.strokeStyle = "red";
+    ctx.lineWidth = 5;
+    ctx.strokeRect(100,100,200,500);
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 2
+    ctx.strokeRect(200,200,200,500); */
+
+});
+
